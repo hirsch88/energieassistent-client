63c0f5836932be759af0afc69ccb2cdfd804e3ba
Hirschfeld Gerhard, ENT-DES-BPE-DI-WUI <Gerhard.Hirschfeld@swisscom.com>
2017-08-21 14:25:16 +0200

    Add basic chart to the overview

--- src/components/overview/overview.js
+++ src/components/overview/overview.js
@@ -1,34 +1,47 @@
 //import {computedFrom} from 'aurelia-framework';
+import Chart from 'chart.js';
 
-export class Welcome {
-  heading = 'Welcome to the Aurelia Navigation App!';
-  firstName = 'John';
-  lastName = 'Doe';
-  previousValue = this.fullName;
+export class Overview {
 
-  //Getters can't be directly observed, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
-  //as well as the corresponding import above.
-  //@computedFrom('firstName', 'lastName')
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
+  attached() {
+    var ctx = $("#myChart");
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["\uf007", "\uf073", "\uf0fe"],
+        datasets: [{
+          data: [4, 8, 12],
+          backgroundColor: [
+            '#fcc419',
+            '#f59f00',
+            '#e67700'
+          ]
+        }]
+      },
+      options: {
+        tooltips: {
+          titleFontSize: 24,
+          titleFontFamily: 'FontAwesome'
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 16
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              fontFamily: 'FontAwesome',
 
-  submit() {
-    this.previousValue = this.fullName;
-    alert(`Welcome, ${this.fullName}!`);
-  }
+            }
+          }]
+        }
+      }
+    });
 
-  canDeactivate() {
-    if (this.fullName !== this.previousValue) {
-      return confirm('Are you sure you want to leave?');
-    }
-  }
-}
-
-export class UpperValueConverter {
-  toView(value) {
-    return value && value.toUpperCase();
   }
 }
