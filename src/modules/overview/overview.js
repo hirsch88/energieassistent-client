b21de638c18bd7680f0cf591596edabf104eddda
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-10-16 21:29:42 +0200

    Add c3 as a chart lib

--- src/modules/overview/overview.js
+++ src/modules/overview/overview.js
@@ -58,33 +58,33 @@ export class Overview {
         this.energyOptions.data = this.energyService.GetOverviewDataWeeks;
         this.heatOptions.data = this.heatService.GetOverviewDataWeeks;
         this.waterOptions.data = this.waterService.GetOverviewDataWeeks;
-        this.data.averageEnergy = 1250;
-        this.data.averageHeat = 2800;
-        this.data.averageWater = 1800;
+        this.energyOptions.average = 1250;
+        this.heatOptions.average = 2800;
+        this.waterOptions.average = 1800;
         break;
       case "month":
         this.energyOptions.data = this.energyService.GetOverviewDataMonth;
         this.heatOptions.data = this.heatService.GetOverviewDataMonth;
         this.waterOptions.data = this.waterService.GetOverviewDataMonth;
-        this.data.averageEnergy = 1250 * 4;
-        this.data.averageHeat = 2800 * 4;
-        this.data.averageWater = 1800 * 4;
+        this.energyOptions.average = 1250 * 4;
+        this.heatOptions.average = 2800 * 4;
+        this.waterOptions.average = 1800 * 4;
         break;
       case "quarter":
         this.energyOptions.data = this.energyService.GetOverviewDataQuarter;
         this.heatOptions.data = this.heatService.GetOverviewDataQuarter;
         this.waterOptions.data = this.waterService.GetOverviewDataQuarter;
-        this.data.averageEnergy = 1250 * 12;
-        this.data.averageHeat = 2800 * 12;
-        this.data.averageWater = 1800 * 12;
+        this.energyOptions.average = 1250 * 12;
+        this.heatOptions.average = 2800 * 12;
+        this.waterOptions.average = 1800 * 12;
         break;
       default:
         this.energyOptions.data = this.energyService.GetOverviewDataWeeks;
         this.heatOptions.data = this.heatService.GetOverviewDataWeeks;
         this.waterOptions.data = this.waterService.GetOverviewDataWeeks;
-        this.data.averageEnergy = 1250;
-        this.data.averageHeat = 2800;
-        this.data.averageWater = 1800;
+        this.energyOptions.average = 1250;
+        this.heatOptions.average = 2800;
+        this.waterOptions.average = 1800;
     }
 
     var moodWarningThreshold = 10;
@@ -127,57 +127,6 @@ export class Overview {
     this.energyOptions.status = getStatus(this.energyOptions);
     this.heatOptions.status = getStatus(this.heatOptions);
     this.waterOptions.status = getStatus(this.waterOptions);
-
-    // if (this.data.trendEnergy < 0) {
-    //   //green
-    //   this.data.moodEnergy = "smile-good";
-    //   this.data.iconEnergy = "fa fa-smile-o";
-    //   this.data.tooltipEnergy = "Sie haben " + Math.abs(this.data.trendEnergy) + "% weniger Strom verbraucht";
-    // } else if (this.data.trendEnergy < moodWarningThreshold) {
-    //   //orange
-    //   this.data.moodEnergy = "smile-neutral";
-    //   this.data.iconEnergy = "fa fa-meh-o";
-    //   this.data.tooltipEnergy = "Sie haben " + this.data.trendEnergy + "% mehr Strom verbraucht";
-    // } else {
-    //   //red
-    //   this.data.moodEnergy = "smile-bad";
-    //   this.data.iconEnergy = "fa fa-frown-o";
-    //   this.data.tooltipEnergy = "Sie haben " + this.data.trendEnergy + "% mehr Strom verbraucht";
-    // }
-
-    // if (this.data.trendHeat < 0) {
-    //   //green
-    //   this.data.moodHeat = "smile-good";
-    //   this.data.iconHeat = "fa fa-smile-o";
-    //   this.data.tooltipHeat = "Sie haben " + Math.abs(this.data.trendHeat) + "% weniger Wärme verbraucht";
-    // } else if (this.data.trendHeat < moodWarningThreshold) {
-    //   //orange
-    //   this.data.moodHeat = "smile-neutral";
-    //   this.data.iconHeat = "fa fa-meh-o";
-    //   this.data.tooltipHeat = "Sie haben " + this.data.trendHeat + "% mehr Wärme verbraucht";
-    // } else {
-    //   //red
-    //   this.data.moodHeat = "smile-bad";
-    //   this.data.iconHeat = "fa fa-frown-o";
-    //   this.data.tooltipHeat = "Sie haben " + this.data.trendHeat + "% mehr Wärme verbraucht";
-    // }
-
-    // if (this.data.trendWater < 0) {
-    //   //green
-    //   this.data.moodWater = "smile-good";
-    //   this.data.iconWater = "fa fa-smile-o";
-    //   this.data.tooltipWater = "Sie haben " + Math.abs(this.data.trendWater) + "% weniger Wasser verbraucht";
-    // } else if (this.data.trendWater < moodWarningThreshold) {
-    //   //orange
-    //   this.data.moodWater = "smile-neutral";
-    //   this.data.iconWater = "fa fa-meh-o";
-    //   this.data.tooltipWater = "Sie haben " + this.data.trendWater + "% mehr Wasser verbraucht";
-    // } else {
-    //   //red
-    //   this.data.moodWater = "smile-bad";
-    //   this.data.iconWater = "fa fa-frown-o";
-    //   this.data.tooltipWater = "Sie haben " + this.data.trendWater + "% mehr Wasser verbraucht";
-    // }
 
     // //timer für pulse animated
     // var nodes = document.getElementsByClassName('smile-bad');
--- src/resources/elements/overview-chart.element.js
+++ src/resources/elements/overview-chart.element.js
@@ -1,59 +1,153 @@
 import { bindable, useView } from 'aurelia-framework';
-import Chart from 'chart.js';
+import $ from 'jquery';
+import d3 from 'd3';
+import c3 from 'c3';
 
 @useView('./overview-chart.element.html')
 export class OverviewChartCustomElement {
 
+  LABEL_CH = () => `Schweizer Durchschnitt`;
+  LABEL_NOW = () => `Aktueller Verbrauch`;
+  LABEL_HISTORY = (y) => `Verbrauch ${y}`;
+
   @bindable options;
   @bindable selection;
 
-  chartElement;
-  chart
+  templateElement;
+
+  isAttached = false;
+
+  chartContainer;
+  chart;
+
+  build() {
+    var colors = {
+      blue: {
+        [this.LABEL_NOW()]: 'rgba(114, 195, 252, 0.37)',
+        [this.LABEL_HISTORY(this.options.data[1][0].year)]: '#1862ab',
+        [this.LABEL_CH()]: '#adb5bd'
+      },
+      red: {
+        [this.LABEL_NOW()]: 'rgba(255, 168, 168, 0.37)',
+        [this.LABEL_HISTORY(this.options.data[1][0].year)]: '#c92a2a',
+        [this.LABEL_CH()]: '#adb5bd'
+      },
+      yellow: {
+        [this.LABEL_NOW()]: 'rgba(255, 224, 102, 0.37)',
+        [this.LABEL_HISTORY(this.options.data[1][0].year)]: '#e67700',
+        [this.LABEL_CH()]: '#adb5bd'
+      }
+    }
+
+    var config = {
+      bindto: this.chartContainer,
+      axis: {
+        x: {
+          type: 'category'
+        }
+      },
+      data: {
+        type: 'bar',
+        x: 'x',
+        columns: [],
+        types: {},
+        //   data2: 'line',
+        //   data3: 'line'
+        // },
+        colors: colors[this.options.color] || {}
+      }
+    };
+
+    config.data.types[this.LABEL_CH()] = 'line';
+    config.data.types[this.LABEL_HISTORY(this.options.data[1][0].year)] = 'line';
+
+    this.chart = c3.generate(config);
+  }
 
   optionsChanged(value) {
-    console.info('OverviewChartCustomElement', value);
-    if (!this.chart) {
-      this.buildChart();
-    }
+    if (value) {
+      if (!this.isAttached) {
+        this.build();
+      }
 
+      console.info('optionsChanged', value.data);
+      var x = ['x'];
+      var d1 = ['Aktueller Verbrauch'];
+      var d2 = [`Verbrauch ${this.options.data[1][0].year}`];
+      var d3 = ['Schweizer Durchschnitt', this.options.average, this.options.average, this.options.average, this.options.average];
 
-  }
+      value.data[0].forEach(e => d1.push(e.value));
+      value.data[1].forEach(e => d2.push(e.value));
+      value.data[0].forEach((e) => x.push(e.getLabel(this.selection)));
 
-  buildChart() {
-    if (this.options && this.options.data && this.selection) {
-      this.chart = new Chart($(this.chartElement), {
-        type: 'bar',
-        options: {
-          title: {
-            display: true,
-            fontSize: 12
-          },
-          tooltips: {
-            titleFontSize: 12,
-            titleFontFamily: 'Open Sans'
-          },
-          legend: {
-            position: 'bottom',
-            fullWidth: false
-          },
-          scales: {
-            yAxes: [{
-              ticks: {
-                min: 0
-              }
-            }],
-            xAxes: [{
-              ticks: {
-                fontFamily: 'Open Sans',
-              }
-            }]
-          }
-        },
-        data: {
-          labels: [this.options.data[0][3].getLabel(this.selection), this.options.data[0][2].getLabel(this.selection), this.options.data[0][1].getLabel(this.selection), this.options.data[0][0].getLabel(this.selection)],
-        }
-      });
+      this.chart.load({
+        columns: [
+          d1, d2, d3, x
+        ]
+      })
+
     }
   }
+
+
+  // buildChart() {
+  //   if (this.options && this.options.data && this.selection) {
+  //     setTimeout(() => {
+  //       this.chart = new Chart($(this.chartContainer).find('canvas'), {
+  //         type: 'bar',
+  //         options: {
+  //           title: {
+  //             display: true,
+  //             fontSize: 12
+  //           },
+  //           tooltips: {
+  //             titleFontSize: 12,
+  //             titleFontFamily: 'Open Sans'
+  //           },
+  //           legend: {
+  //             position: 'bottom',
+  //             fullWidth: true
+  //           },
+  //           scales: {
+  //             yAxes: [{
+  //               ticks: {
+  //                 min: 0
+  //               }
+  //             }],
+  //             xAxes: [{
+  //               ticks: {
+  //                 fontFamily: 'Open Sans',
+  //               }
+  //             }]
+  //           }
+  //         },
+  //         data: {
+  //           labels: [this.options.data[0][3].getLabel(this.selection), this.options.data[0][2].getLabel(this.selection), this.options.data[0][1].getLabel(this.selection), this.options.data[0][0].getLabel(this.selection)],
+  //           datasets: [{
+  //             label: "Aktueller Verbrauch",
+  //             data: [Math.round(100 * this.options.data[0][3].value) / 100, Math.round(100 * this.options.data[0][2].value) / 100, Math.round(100 * this.options.data[0][1].value) / 100, Math.round(100 * this.options.data[0][0].value) / 100],
+  //             backgroundColor: 'rgba(255, 224, 102, 0.37)',
+  //             borderColor: '#ffe066',
+  //             borderWidth: 2
+  //           }, {
+  //             label: "Verbrauch " + this.options.data[1][0].year,
+  //             data: [Math.round(100 * this.options.data[1][3].value) / 100, Math.round(100 * this.options.data[1][2].value) / 100, Math.round(100 * this.options.data[1][1].value) / 100, Math.round(100 * this.options.data[1][0].value) / 100],
+  //             borderColor: "#e67700",
+  //             backgroundColor: "rgba(230, 119, 0, 0.36)",
+  //             fill: false,
+  //             type: 'line'
+  //           }, {
+  //             label: "Schweizer Durchschnitt",
+  //             data: [this.options.average, this.options.average, this.options.average, this.options.average],
+  //             borderColor: "#adb5bd",
+  //             backgroundColor: "rgba(173, 181, 189, 0.36)",
+  //             fill: false,
+  //             type: 'line'
+  //           }]
+  //         }
+  //       });
+  //     }, 500);
+  //   }
+  // }
 
 }
