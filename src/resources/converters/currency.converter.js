9905c2993ef2b2234ef2d485a89cedab885d6ae1
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-10-18 16:41:35 +0200

    Add award layout and add cost table

---
+++ src/resources/converters/currency.converter.js
@@ -0,0 +1,7 @@
+import * as numeral from 'numeraljs';
+
+export class CurrencyValueConverter {
+  toView(value) {
+    return numeral((Math.ceil(value * 20) / 20).toFixed(2)).format('0,0.00')
+  }
+}
--- src/resources/converters/index.js
+++ src/resources/converters/index.js
@@ -3,6 +3,6 @@ import { PLATFORM } from 'aurelia-framework';
 export function configure(aurelia) {
   aurelia
 		.globalResources([
-      // PLATFORM.moduleName('./name/name.converter')
+      PLATFORM.moduleName('./currency.converter'),
     ]);
 }
