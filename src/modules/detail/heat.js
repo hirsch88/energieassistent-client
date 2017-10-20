da169c8e1ba0d882df7b383ff854fb271bb943b9
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-10-20 16:50:52 +0200

    Add heat detail page

--- src/modules/detail/heat.js
+++ src/modules/detail/heat.js
@@ -1,3 +1,194 @@
+import * as moment from 'moment';
+
+import { inject, observable } from 'aurelia-framework';
+import { HeatService } from '../../services/heat.service';
+
+@inject(HeatService)
 export class Heat {
 
+  static SwissAverageCosts = 100;
+  static SwissAverageHeat = 100;
+  static Months = [
+    'Jan',
+    'Feb',
+    'Mär',
+    'Apr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Okt',
+    'Nov',
+    'Dez'
+  ];
+
+  static Quarters = {
+    1: [1, 2, 3],
+    2: [4, 5, 6],
+    3: [7, 8, 9],
+    4: [10, 11, 12]
+  };
+
+  @observable dataHeat;
+
+  @observable chatOptionsTotal;
+  @observable chatOptionsNormal;
+  @observable chatOptionsLow;
+
+  @observable pieCostsOptions;
+  @observable pieMediumOptions;
+
+  @observable selection;
+
+  constructor(heatService) {
+    this.heatService = heatService;
+    this.data = {};
+  }
+
+  attached() {
+    this.pieCostsOptions = {
+      color: 'red',
+      data: [
+        {
+          color: '#c92a2a',
+          value: 50,
+          text: 'Gas'
+        },
+        {
+          color: '#ffa8a8',
+          value: 30,
+          text: 'Dienstleistung'
+        },
+        {
+          color: '#adb5bd',
+          value: 20,
+          text: 'Abgaben'
+        }
+      ]
+    };
+  }
+
+  onChange(value, type) {
+    this.selection = { value, type };
+
+    if (!value || !type) {
+      return;
+    }
+
+    this.setTotalChartOptions();
+  }
+
+  getDataForAnYear(key) {
+    this.dataHeat = this.heatService.GetDetailDataYear;
+    let data = this.heatService.GetData;
+    let now = data.filter(d => {
+      return `${d.year}` === this.selection.value
+    });
+    let past = data.filter(d => {
+      return `${(parseInt(d.year) + 1)}` === this.selection.value
+    });
+
+    let d1 = ['now'];
+    let d2 = ['past'];
+    let x = ['x'];
+    for (let i = 1; i < 13; i++) {
+      d1[i] = 0;
+      d2[i] = 0;
+      x[i] = Heat.Months[i - 1];
+    }
+
+    d1 = now.reduce((accumulator, currentValue) => {
+      accumulator[currentValue.month] = (accumulator[currentValue.month] || 0) + parseInt(currentValue[key], 10);
+      return accumulator;
+    }, d1);
+
+    d2 = past.reduce((accumulator, currentValue) => {
+      accumulator[currentValue.month] = (accumulator[currentValue.month] || 0) + parseInt(currentValue[key], 10);
+      return accumulator;
+    }, d2);
+
+    for (let n = 1; n < d1.length; n++) {
+      d1[n] = parseFloat(d1[n], 10).toFixed(2);
+      d2[n] = parseFloat(d2[n], 10).toFixed(2);
+    }
+
+    return [d1, d2, x];
+  }
+
+  getDataForAnQuarter(key) {
+    this.dataHeat = this.heatService.GetDetailDataQuarter;
+    let data = this.heatService.GetData;
+
+    let y = parseInt(this.selection.value.split('-')[1], 10);
+    let q = parseInt(this.selection.value.split('Q')[1].split('-')[0], 10);
+    let ms = Heat.Quarters[q];
+
+    let qs = data.filter(d => {
+      return ms.indexOf(d.month) >= 0;
+    });
+
+    let now = qs.filter(d => d.year === y)
+    let past = qs.filter(d => (d.year + 1) === y);
+
+    let d1 = ['now'];
+    let d2 = ['past'];
+    let x = ['x'];
+    let max = (past.length > now.length) ? past.length : now.length;
+    for (let i = 1; i < max; i++) {
+      d1[i] = 0;
+      d2[i] = 0;
+    }
+
+    for (let n = 0; n < past.length; n++) {
+      d2[n + 1] = past[n][key]
+    }
+
+    for (let n = 0; n < now.length; n++) {
+      d1[n + 1] = now[n][key]
+    }
+
+    x[1] = now[1].week;
+    for (let n = 2; n < max; n++) {
+      x[n] = x[n - 1] + 1;
+    }
+
+    for (let n = 1; n < d1.length; n++) {
+      d1[n] = parseFloat(d1[n], 10).toFixed(2);
+      d2[n] = parseFloat(d2[n], 10).toFixed(2);
+    }
+
+    return [d1, d2, x];
+  }
+
+  setTotalChartOptions() {
+    let data, dataLow, dataNormal;
+    switch (this.selection.type) {
+      case 'quarter':
+        data = this.getDataForAnQuarter('value');
+        dataLow = this.getDataForAnQuarter('valueLow');
+        dataNormal = this.getDataForAnQuarter('valueNormal');
+        break;
+      case 'year':
+        data = this.getDataForAnYear('value');
+        dataLow = this.getDataForAnYear('valueLow');
+        dataNormal = this.getDataForAnYear('valueNormal');
+        break;
+    }
+
+    this.chartOptionsTotal = {
+      color: 'red',
+      data
+    };
+
+    this.chartOptionsLow = {
+      color: 'red',
+      data: dataLow
+    };
+
+    this.chartOptionsNormal = {
+      color: 'red',
+      data: dataNormal
+    };
+  }
 }
--- src/services/heat.service.js
+++ src/services/heat.service.js
@@ -11,7 +11,7 @@ export class HeatService {
     while (!(pointer.year() === start.year() && pointer.week() === start.week())) {
       this.data.push(new DataItem(
         pointer.year(),
-        pointer.month()+1,
+        pointer.month() + 1,
         pointer.week(),
         (Math.random() * (5000 - 500) + 0.05).toFixed(2)
       ));
@@ -20,9 +20,9 @@ export class HeatService {
   }
 
   get getISOWeeks() {
-    var y = new Date().getFullYear()-1;
+    var y = new Date().getFullYear() - 1;
     var d,
-        isLeap;
+      isLeap;
 
     d = new Date(y, 0, 1);
     isLeap = new Date(y, 1, 29).getMonth() === 1;
@@ -33,13 +33,31 @@ export class HeatService {
   }
 
   getSumTotal(total, ele) {
-    return total +  Number(ele.value);
+    return total + Number(ele.value);
   }
 
   get GetOverviewDataWeeks() {
     var a = [];
     a.push(this.data.filter((_, index) => index < 4));
-    a.push(this.data.filter((_,index) => index < 4+this.getISOWeeks && index >= this.getISOWeeks));
+    a.push(this.data.filter((_, index) => index < 4 + this.getISOWeeks && index >= this.getISOWeeks));
+    return a;
+  }
+
+  get GetData() {
+    return this.data.reverse();
+  }
+
+  get GetDetailDataYear() {
+    var a = [];
+    a.push(this.data.filter((_, index) => index < 52));
+    a.push(this.data.filter((_, index) => index < 52 + this.getISOWeeks && index >= this.getISOWeeks));
+    return a;
+  }
+
+  get GetDetailDataQuarter() {
+    var a = [];
+    a.push(this.data.filter((_, index) => index < 13));
+    a.push(this.data.filter((_, index) => index < 13 + this.getISOWeeks && index >= this.getISOWeeks));
     return a;
   }
 
@@ -51,15 +69,15 @@ export class HeatService {
     var firstDay = new Date(y, m, 1);
     var pointer1 = moment(firstDay);
     var pointer2 = moment(firstDay);
-    pointer2.subtract(1,'year');
-    for(let i = 0; i<=3 ;i++) {
+    pointer2.subtract(1, 'year');
+    for (let i = 0; i <= 3; i++) {
       var dataMonthThisYear = this.data.filter((e, i) => {
         var d = new Date(e.year, e.month, 1);
         return d.getTime() == pointer1.valueOf();
       });
       thisYear.push(new DataItem(
         pointer1.year(),
-        pointer1.month()+1,
+        pointer1.month() + 1,
         pointer1.week(),
         dataMonthThisYear.reduce(this.getSumTotal, 0)
       ));
@@ -69,33 +87,33 @@ export class HeatService {
       });
       lastYear.push(new DataItem(
         pointer2.year(),
-        pointer2.month()+1,
+        pointer2.month() + 1,
         pointer2.week(),
         dataMonthLastYear.reduce(this.getSumTotal, 0)
       ));
-      pointer1.subtract(1,'month');
-      pointer2.subtract(1,'month');
+      pointer1.subtract(1, 'month');
+      pointer2.subtract(1, 'month');
     }
     a.push(thisYear);
     a.push(lastYear);
     return a;
   }
 
-  get GetOverviewDataQuarter(){
+  get GetOverviewDataQuarter() {
     var a = [];
     var thisYear = [];
     var lastYear = [];
     var pointer1 = moment();
     var pointer2 = moment();
-    pointer2.subtract(1,'year');
-    for(let i = 0; i<=3 ;i++) {
+    pointer2.subtract(1, 'year');
+    for (let i = 0; i <= 3; i++) {
       var dataThisYear = this.data.filter((e, i) => {
         var d = new Date(e.year, e.month, 1);
         return d.getTime() >= pointer1.startOf('quarter').valueOf() && d.getTime() <= pointer1.endOf('quarter').valueOf();
       });
       thisYear.push(new DataItem(
         pointer1.year(),
-        pointer1.month()+1,
+        pointer1.month() + 1,
         pointer1.week(),
         dataThisYear.reduce(this.getSumTotal, 0)
       ));
@@ -105,12 +123,12 @@ export class HeatService {
       });
       lastYear.push(new DataItem(
         pointer2.year(),
-        pointer2.month()+1,
+        pointer2.month() + 1,
         pointer2.week(),
         dataLastYear.reduce(this.getSumTotal, 0)
       ));
-      pointer1.subtract(1,'quarter');
-      pointer2.subtract(1,'quarter');
+      pointer1.subtract(1, 'quarter');
+      pointer2.subtract(1, 'quarter');
     }
     a.push(thisYear);
     a.push(lastYear);
